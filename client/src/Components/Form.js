b270bd4e545b15943c0b950e647736879205f700
alison-martinez <alison.a.martinez@gmail.com>
2023-02-01 07:30:20 -0500

    display and add new products

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -1,36 +1,48 @@
 import React, { useState } from 'react';
 
-const Form = () => {
-
+const Form = ({onSubmit}) => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+  
   const handleClick = (e)=> {
     e.preventDefault();
     const form = document.querySelector('.add-form');
     form.classList.toggle('visible');
   };
+
+  const handleAddForm = (e)=> {
+    handleClick(e)
+    onSubmit({
+      title,
+      price: parseInt(price, 10),
+      quantity: parseInt(quantity, 10)
+    })
+  };
   
   return ( 
-    <div class="add-form">
-      <p><a href="#" onClick={handleClick} class="button add-product-button">Add A Product</a></p>
+    <div className="add-form">
+      <p><a href="#" onClick={handleClick} className="button add-product-button">Add A Product</a></p>
         <h3>Add Product</h3>
         <form>
-          <div class="input-group">
-            <label for="product-name">Product Name</label>
-            <input type="text" id="product-name" value=""/>
+          <div className="input-group">
+            <label htmlFor="product-name">Product Name</label>
+            <input value={title} type="text" id="product-name"  onChange={(e)=> setTitle(e.target.value)}/>
           </div>
 
-          <div class="input-group">
-            <label for="product-price">Price</label>
-            <input type="text" id="product-price" value=""/>
+          <div className="input-group">
+            <label htmlFor="product-price">Price</label>
+            <input value={price} type="text" id="product-price" onChange={(e) => setPrice(e.target.value)}/>
           </div>
 
-          <div class="input-group">
-            <label for="product-quantity">Quantity</label>
-            <input type="text" id="product-quantity" value=""/>
+          <div className="input-group">
+            <label htmlFor="product-quantity">Quantity</label>
+            <input value={quantity} type="text" id="product-quantity" onChange={(e)=> setQuantity(e.target.value)}/>
           </div>
 
-          <div class="actions form-actions">
-            <a class="button">Add</a>
-            <a class="button" onClick={handleClick} >Cancel</a>
+          <div className="actions form-actions">
+            <a className="button" onClick={handleAddForm}>Add</a>
+            <a className="button" onClick={handleClick} >Cancel</a>
           </div>
         </form>
       </div>
--- client/src/Components/ProductList.js
+++ client/src/Components/ProductList.js
@@ -3,11 +3,11 @@ import Product from "./Product";
 
 const ProductList = ({products}) => {
   return (
-    <div class="product-listing">
+    <div className="product-listing">
       <h2>Products</h2>
-      {products.map(product => {
+      {products.map((product) => {
         return(
-          <Product key={product.id} {...product}/>
+          <Product {...product} key={product._id} />
         )
       })}
     </div>
