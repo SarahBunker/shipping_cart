285e1e1360d8c642afccec31821e2cf6aeaed204
alison-martinez <alison.a.martinez@gmail.com>
2023-02-05 21:34:33 -0500

    jest testing AddProductForm

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Form = ({onSubmit}) => {
+const Form = ({ onSubmit }) => {
   const [title, setTitle] = useState("");
   const [price, setPrice] = useState("");
   const [quantity, setQuantity] = useState("");
   const [isFormVisible, setIsFormVisible] = useState(false);
-  
-  const handleClick = (e)=> {
+
+  const handleClick = (e) => {
     e.preventDefault();
     setIsFormVisible(!isFormVisible);
   };
 
-  const handleAddForm = (e)=> {
+  const handleAddForm = (e) => {
     e.preventDefault();
     if (title === "") return;
     if (quantity === "") {
-      setQuantity(0)
+      setQuantity(0);
     }
     if (price === "") return;
-    onSubmit({
-      title,
-      price: parseInt(price, 10),
-      quantity: parseInt(quantity, 10)
-    }, resetInputs)
-    
+    onSubmit(
+      {
+        title,
+        price: parseInt(price, 10),
+        quantity: parseInt(quantity, 10),
+      },
+      resetInputs
+    );
   };
 
   const resetInputs = () => {
@@ -32,34 +34,60 @@ const Form = ({onSubmit}) => {
     setQuantity("");
     setIsFormVisible(!isFormVisible);
   };
-  
-  return ( 
-    <div className={`add-form ${isFormVisible ? "visible" : ""}`}>
-      <p><a href="#" onClick={handleClick} className="button add-product-button">Add A Product</a></p>
-        <h3>Add Product</h3>
-        <form>
-          <div className="input-group">
-            <label htmlFor="product-name">Product Name</label>
-            <input value={title} type="text" id="product-name"  onChange={(e)=> setTitle(e.target.value)}/>
-          </div>
 
-          <div className="input-group">
-            <label htmlFor="product-price">Price</label>
-            <input value={price} type="text" id="product-price" onChange={(e) => setPrice(e.target.value)}/>
-          </div>
+  return (
+    <div
+      data-testid="container"
+      className={`add-form ${isFormVisible ? "visible" : ""}`}
+    >
+      <p>
+        <a href="#" onClick={handleClick} className="button add-product-button">
+          Add A Product
+        </a>
+      </p>
+      <h3>Add Product</h3>
+      <form>
+        <div className="input-group">
+          <label htmlFor="product-name">Product Name</label>
+          <input
+            value={title}
+            type="text"
+            id="product-name"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
 
-          <div className="input-group">
-            <label htmlFor="product-quantity">Quantity</label>
-            <input value={quantity} type="text" id="product-quantity" onChange={(e)=> setQuantity(e.target.value)}/>
-          </div>
+        <div className="input-group">
+          <label htmlFor="product-price">Price</label>
+          <input
+            value={price}
+            type="text"
+            id="product-price"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
 
-          <div className="actions form-actions">
-            <a className="button" onClick={handleAddForm}>Add</a>
-            <a className="button" onClick={handleClick} >Cancel</a>
-          </div>
-        </form>
-      </div>
-   );
-}
- 
+        <div className="input-group">
+          <label htmlFor="product-quantity">Quantity</label>
+          <input
+            value={quantity}
+            type="text"
+            id="product-quantity"
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+        </div>
+
+        <div className="actions form-actions">
+          <a data-testid="add" className="button" onClick={handleAddForm}>
+            Add
+          </a>
+          <a className="button" onClick={handleClick}>
+            Cancel
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
 export default Form;
---
+++ client/src/__tests__/AddProductForm.js
@@ -0,0 +1,119 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import Form from "../Components/Form";
+
+test("add product link is shown on the page", () => {
+  render(<Form />);
+  const link = screen.getByRole("link");
+  expect(link).toBeInTheDocument();
+});
+
+test("form becomes visible when link is clicked", async () => {
+  //const func = jest.fn();
+  render(<Form />);
+  const user = userEvent.setup();
+  const link = screen.getByRole("link");
+  const div = screen.getByTestId("container");
+
+  await user.click(link);
+  expect(div.classList.contains("visible")).toBe(true);
+});
+
+test("name state changes on input", async () => {
+  render(<Form />);
+  const user = userEvent.setup();
+  const link = screen.getByRole("link");
+
+  await user.click(link);
+  const inputProductName = screen.getByRole("textbox", {
+    name: "Product Name",
+  });
+  await user.type(inputProductName, "chocolate");
+  expect(inputProductName).toHaveValue("chocolate");
+});
+
+test("price state changes on input", async () => {
+  render(<Form />);
+  const user = userEvent.setup();
+  const link = screen.getByRole("link");
+
+  await user.click(link);
+  const inputPrice = screen.getByRole("textbox", {
+    name: "Price",
+  });
+  await user.type(inputPrice, "5");
+  expect(inputPrice).toHaveValue("5");
+});
+
+test("quantity state changes on input", async () => {
+  render(<Form />);
+  const user = userEvent.setup();
+  const link = screen.getByRole("link");
+
+  await user.click(link);
+  const inputProductQuantity = screen.getByRole("textbox", {
+    name: "Quantity",
+  });
+  await user.type(inputProductQuantity, "300");
+  expect(inputProductQuantity).toHaveValue("300");
+});
+
+test("onSubmit is called when button is clicked", async () => {
+  const func = jest.fn();
+  render(<Form onSubmit={func} />);
+  const user = userEvent.setup();
+  const link = screen.getByRole("link");
+  await user.click(link);
+
+  const inputProductName = screen.getByRole("textbox", {
+    name: "Product Name",
+  });
+  await user.type(inputProductName, "chocolate");
+
+  const inputPrice = screen.getByRole("textbox", {
+    name: "Price",
+  });
+  await user.type(inputPrice, "5");
+
+  const button = screen.getByTestId("add");
+  await user.click(button);
+  expect(func.mock.calls.length).toBe(1);
+});
+
+test("onSubmit is called with the new product", async () => {
+  const func = jest.fn();
+  render(<Form onSubmit={func} />);
+  const user = userEvent.setup();
+  const link = screen.getByRole("link");
+  await user.click(link);
+
+  const inputProductName = screen.getByRole("textbox", {
+    name: "Product Name",
+  });
+  await user.type(inputProductName, "chocolate");
+
+  const inputPrice = screen.getByRole("textbox", {
+    name: "Price",
+  });
+
+  const inputProductQuantity = screen.getByRole("textbox", {
+    name: "Quantity",
+  });
+  await user.type(inputProductQuantity, "12");
+
+  await user.type(inputPrice, "5");
+  const newProduct = {
+    title: inputProductName.value,
+    price: parseInt(inputPrice.value, 10),
+    quantity: parseInt(inputProductQuantity.value, 12),
+  };
+
+  const button = screen.getByTestId("add");
+  await user.click(button);
+  expect(func.mock.calls[0][0]).toEqual(newProduct);
+});
